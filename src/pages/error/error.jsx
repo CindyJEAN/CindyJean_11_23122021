7b32edc4e8eaf97d32b5735f107efe9f0f314d71
Cindy Jean <cindy.jean83@gmail.com>
2022-01-13 10:58:18 +0100

    feat: implémentation page error

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,14 +1,21 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
 export default class Error extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     console.log(props);
   }
 
-  render(){
+  render() {
     return (
-      <div>404</div>
+      <section className="errorComponent">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="link">
+          Retourner sur la page d'accueil
+        </Link>
+      </section>
     );
   }
 }
